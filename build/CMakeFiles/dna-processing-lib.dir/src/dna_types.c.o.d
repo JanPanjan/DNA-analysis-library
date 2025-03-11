a1b61adf0a5df197c875bbb7e529b6a541c1965e
JanPanjan <89231282@student.upr.si>
2025-03-11 07:59:13 +0100

    uhh

--- build/CMakeFiles/dna-processing-lib.dir/src/dna_types.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/dna-processing-lib.dir/src/dna_types.c.o: \
- /home/pogacha/progAAAAAAA/LPL/C/projects/dna-processing-lib/src/dna_types.c \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/dna-processing-lib.dir/src/dna_utils.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/dna-processing-lib.dir/src/dna_utils.c.o: \
- /home/pogacha/progAAAAAAA/LPL/C/projects/dna-processing-lib/src/dna_utils.c \
- /usr/include/stdc-predef.h
